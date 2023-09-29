61fac97794261c136e552a8608ac13d127bb228d
Claudio Pereira <claudios.pereira@gmail.com>
2023-09-29 11:42:16 -0300

    Commit do web_app na pasta Docker.

---
+++ docker/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const os = require('os');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Informações do Host</h1>');
+  res.write(`<p>Nome do Host: ${os.hostname()}</p>`);
+  res.write(`<p>Endereço IP: ${getIpAddress()}</p>`);
+  res.write(`<p>Hora do Host: ${getCurrentTime()}</p>`);
+  res.end();
+});
+
+function getIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const ifaceName in interfaces) {
+    const iface = interfaces[ifaceName];
+    for (const alias of iface) {
+      if (alias.family === 'IPv4' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return 'Endereço IP não encontrado';
+}
+
+function getCurrentTime() {
+  const now = new Date();
+  return now.toLocaleString();
+}
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor está rodando em http://localhost:${port}`);
+});
