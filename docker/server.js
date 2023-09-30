aaa16201fa31af4eed5e76af72c53d66fa8b4202
Claudio Pereira <claudios.pereira@gmail.com>
2023-09-29 22:25:40 -0300

    Commit outra mudança no app.

--- docker/server.js
+++ docker/server.js
@@ -1,35 +1,17 @@
-const http = require('http');
-const os = require('os');
+'use strict';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Informações do Pod Kubernetes</h1>');
-  res.write(`<p>Nome do Pod: ${process.env.HOSTNAME}</p>`);
-  res.write(`<p>Endereço IP do Pod: ${getPodIpAddress()}</p>`);
-  res.write(`<p>Hora do Servidor: ${getCurrentTime()}</p>`);
-  res.end();
+const express = require('express');
+
+// Constants
+const PORT = 30000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('MENSAGEM - COLOQUE AQUI');
 });
 
-function getPodIpAddress() {
-  const networkInterfaces = os.networkInterfaces();
-  const iface = networkInterfaces['eth0'] || networkInterfaces['eth1'] || networkInterfaces['eth2'];
-  if (iface) {
-    for (const alias of iface) {
-      if (alias.family === 'IPv4' && !alias.internal) {
-        return alias.address;
-      }
-    }
-  }
-  return 'Endereço IP não encontrado';
-}
-
-function getCurrentTime() {
-  const now = new Date();
-  return now.toLocaleString();
-}
-
-const port = 30000;
-const host = '0.0.0.0';
-server.listen(port, host, () => {
-  console.log(`Servidor está rodando em http://${host}:${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
